5801f3593ddf27c59d6d48fb311640e96526cfe2
ple41080 <78138749+ple41080@users.noreply.github.com>
2023-03-13 11:37:38 +0700

    no message

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
+import { UsersModule } from './backend/users.module';
+import { UsersController } from './backend/users.controller';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+
+
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    UsersModule, TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'cwie',
+      entities: [],
+      synchronize: true,
+    }),],
+  controllers: [
+    UsersController,
+    UsersController, AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
